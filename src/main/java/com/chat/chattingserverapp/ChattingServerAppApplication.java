9025c44fdf1aa1d876eb1cbdd0a51f35076cbd5d
heosangbeom <luv_3_u@naver.com>
2025-05-30 10:21:15 +0900

    first commit

---
+++ src/main/java/com/chat/chattingserverapp/ChattingServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.chat.chattingserverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChattingServerAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ChattingServerAppApplication.class, args);
+  }
+
+}
